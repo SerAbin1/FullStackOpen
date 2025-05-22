3d39db1604d5a13b5e6b1bae8bdaae9076cb5d10
SerAbinU1 <abinyt1234@gmail.com>
2025-05-22 11:32:02 +0000

    Complete part1

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -35,23 +35,19 @@ const Header = (props) => {
   )
 }
 
-const Part = (props) => {
-  return (
-    <>
-      <p> {props.part} {props.exercises} </p>
-    </>
-  )
-}
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}  />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  )
-}
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
 
 const Total = (props) => {
   return (
