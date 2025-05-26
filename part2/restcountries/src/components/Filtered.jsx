6ec8e8db69ec2bc1768ff1b262539e9169625c95
SerAbinU1 <abinyt1234@gmail.com>
2025-05-26 18:57:27 +0000

    2.20*: Data for countries, step 3 done. Part 2 done

--- part2/restcountries/src/components/Filtered.jsx
+++ part2/restcountries/src/components/Filtered.jsx
@@ -1,5 +1,35 @@
-import { useState } from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
+const api_key = import.meta.env.VITE_SOME_KEY
 
+const GetWeather = ({country}) => {
+  const [weather, setWeather] = useState(null)
+
+  const lat = country.capitalInfo.latlng[0]
+  const lng = country.capitalInfo.latlng[1]
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}`
+      )
+      .then((response) => {
+        setWeather(response.data)
+      })
+  }, [lat, lng])
+
+  if (!weather) return <p>Loading weather...</p>
+
+  const temp = (weather.main.temp - 273.15).toFixed(2)
+  return (
+    <>
+      <h1>Weather in {country.capital}</h1>
+      <p>Temperature {temp} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+      <p>Wind {weather.wind.speed} m/s</p>
+    </>
+  )
+}
 
 const ShowCountry = ({ country }) => (
   <div>
@@ -21,7 +51,10 @@ const Filtered = ({ filtered, error }) => {
 
   if (filtered.length === 1) {
     return (
-      <ShowCountry country={filtered[0]} />
+      <>
+        <ShowCountry country={filtered[0]} />
+        <GetWeather country={filtered[0]} />
+      </>
     )
   }
 
