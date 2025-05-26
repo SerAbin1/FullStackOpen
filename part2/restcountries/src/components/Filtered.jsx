27d3dd0f1774ac34a1e3d3077308176a9d9491bb
SerAbinU1 <abinyt1234@gmail.com>
2025-05-26 16:33:22 +0000

    2.19*: Data for countries, step 2 done

--- part2/restcountries/src/components/Filtered.jsx
+++ part2/restcountries/src/components/Filtered.jsx
@@ -1,28 +1,41 @@
+import { useState } from "react"
+
+
+const ShowCountry = ({ country }) => (
+  <div>
+    <h1>{country.name.common}</h1>
+    <p>{country.capital}</p>
+    <p>Area {country.area}</p>
+    <h1>Languages</h1>
+    {Object.values(country.languages).map((val, index) => (
+      <li key={index}>{val}</li>
+    ))}
+    <img src={country.flags.png} />
+  </div>
+)
+
 const Filtered = ({ filtered, error }) => {
+  const [selected, setSelected] = useState(null)
+
   if (error) return <p>{error}</p>
 
   if (filtered.length === 1) {
-    const flag = filtered[0].flags.png
-    const country = filtered[0]
-
     return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>{country.capital}</p>
-        <p>Area {country.area}</p>
-        <h1>Languages</h1>
-        {Object.values(country.languages).map((val, index) => (
-          <li key={index}>{val}</li>
-        ))}
-        <img src={flag} />
-      </div>
+      <ShowCountry country={filtered[0]} />
     )
   }
+
   else {
     return (
-      filtered.map(country => (
-        <p key={country.cca3}>{country.name.common}</p>
-      ))  
+      <>
+      {filtered.map(country => (
+        <div key={country.cca3}>
+          <p>{country.name.common}</p>
+          <button onClick={() => setSelected(country)}>Show</button>
+        </div>
+      ))}
+      {selected && <ShowCountry country={selected} />}
+      </>
     )
   }
 }
