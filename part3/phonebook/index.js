54d2e7db03a3820afe86c26e1a27fa3012e19711
SerAbinU1 <abinyt1234@gmail.com>
2025-05-28 12:08:26 +0000

    3.9 Phonebook backend step 9 done. Frontend working with backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
-app.use(express.json());
+const cors = require('cors');
 
-app.use(morgan('tiny'))
+const app = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
 
 
 let phonebook = [
     { 
       "id": "1",
-      "name": "Arto Hellas", 
+      "name": "Abin Hellas", 
       "number": "040-123456"
     },
     { 
@@ -82,7 +85,7 @@ app.post('/api/persons', (req, res) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
