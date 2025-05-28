a4a2e36e9a966ba0510f5c8ce586ced478aa7988
SerAbinU1 <abinyt1234@gmail.com>
2025-05-28 08:33:51 +0000

    3.5: Phonebook backend step 5 done.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let phonebook = [
     { 
@@ -52,7 +53,30 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+app.post('/api/persons', (req, res) => {
+  const person = req.body;
+
+  if(!person.name) {
+    return res.status(400).json({ error: "content missing" })
+  }
+
+  const entry = {
+    id: generateId(),
+    name: person.name,
+    number: person.number || 0,
+  }
+
+  phonebook = phonebook.concat(entry)
+
+  res.json(entry)
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
+function generateId() {
+  return Math.floor(Math.random() * 1884348).toString()
+}
