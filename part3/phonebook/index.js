25a96249c58943e78f8305df791d41357ae19aba
SerAbinU1 <abinyt1234@gmail.com>
2025-05-28 16:02:32 +0000

    new branch for render

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(morgan('tiny'));
-app.use(cors());
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
 
 
 let phonebook = [
