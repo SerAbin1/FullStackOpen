61ece407edbe8781881a6e828e2d8b505ac01467
SerAbinU1 <abinyt1234@gmail.com>
2025-07-06 18:35:38 +0000

    6.13 Better Anecdotes, step 11 done

--- part6/anecdotes-redux/src/components/AnecdoteList.jsx
+++ part6/anecdotes-redux/src/components/AnecdoteList.jsx
@@ -1,21 +1,29 @@
 import { useSelector, useDispatch } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer"
+import { setNotificationWithTimeout } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes)
-  const filter = useSelector(state => state.filter)
+  const filter = useSelector((state) => state.filter)
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
   const filteredAnecdotes = sortedAnecdotes.filter((anecdote) =>
     anecdote.content.includes(filter),
   )
   const dispatch = useDispatch()
 
+  const handleVote = (id, content) => {
+    dispatch(vote(id))
+    dispatch(setNotificationWithTimeout(`you voted ${content}`))
+  }
+
   return filteredAnecdotes.map((anecdote) => (
     <div key={anecdote.id}>
       <div>{anecdote.content}</div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+        <button onClick={() => handleVote(anecdote.id, anecdote.content)}>
+          vote
+        </button>
       </div>
     </div>
   ))
--- part6/anecdotes-redux/src/reducers/notificationReducer.js
+++ part6/anecdotes-redux/src/reducers/notificationReducer.js
@@ -2,10 +2,23 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const notificationSlice = createSlice({
   name: "notification",
-  initialState: "initial value for testing",
+  initialState: "",
   reducers: {
-    setNotification(state, action) {},
+    setNotification(state, action) {
+      return action.payload
+    },
+    removeNotification(state, action) {
+      return ""
+    },
   },
 })
+
+const { setNotification, removeNotification } = notificationSlice.actions
+export const setNotificationWithTimeout = (message) => {
+  return async (dispatch) => {
+    dispatch(setNotification(message))
+    setTimeout(() => dispatch(removeNotification()), 5000)
+  }
+}
 
 export default notificationSlice.reducer
